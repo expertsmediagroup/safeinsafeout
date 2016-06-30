229d1373d41dd0f0c4996c8404de92ad93a98f08
root <root@web2.expertsmediagroup.net>
2016-06-30 22:43:46 +0000

    Update Commit

--- library/confirmation.js
+++ library/confirmation.js
@@ -1,5 +1,7 @@
+/*
 $(document).on('device-initialize',function() {
   confirm = function(text) {
     navigator.notification.confirm(text,function(result){alert(result)},'Confirmation',['Cancel','OK']);
   }
 });
+*/
