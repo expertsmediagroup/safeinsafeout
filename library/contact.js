48397fe5baf49d8a27395156889f1d71207df09c
root <root@web2.expertsmediagroup.net>
2016-07-15 15:55:00 +0000

    Update Commit

--- library/contact.js
+++ library/contact.js
@@ -1,4 +1,4 @@
-var interval;
+var interval,contact=[];
 $(document).on('device-initialize device-resume',function() {
   var option,field;
 
@@ -16,11 +16,13 @@ $(document).on('device-initialize device-resume',function() {
 });
 
 $(document).on('device-load device-resume',function() {
-  contact_success_process(); 
+  if($('form').find('[name="contact"]').length || $('span').data('contact')) { 
+    contact_success_interval(); 
+  }
 })
 
 function contact_success(data) {
-  var name='',phone='',contact=[],contact_bypass=[],phoneparser=[];
+  var name='',phone='',contact_bypass=[],phoneparser=[];
   
   for(a=0;a<data.length;a++) {
     if(data[a][navigator.contacts.fieldType.displayName] != null && data[a][navigator.contacts.fieldType.displayName] != undefined) {
@@ -45,14 +47,27 @@ function contact_success(data) {
   interval = true;
 }
 
-function contact_success_process() {
+function contact_success_interval() {
   if(interval) {
-    alert('display');
-    alert(JSON.stringify(contact));
+    if($('span').data('contact')) {
+      for(a=0;a<list.length;a++) {
+        list[a] = list[a].split('|');
+      
+        phoneparser = parsePhone(list[a][1]);
+     
+        if(phoneparser) {
+          phone = '+'+phoneparser.countryCode+'-'+phoneparser.areaCode+'-'+phoneparser.number;
+     
+          $('span[data-contact="'+phone+'"]').html(list[a][0]+' ('+phone+')');
+        }
+      }
+    }
+
+    alert('int');
 
     interval = false;
   } else {
-    setTimeout(contact_success_process,1000);
+    setTimeout(contact_success_interval,1000);
   }
 }
 
